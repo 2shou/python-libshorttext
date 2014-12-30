fed512362062b8718e123bad947fcccfe6745ad3
2shou <gavin.zgz@gmail.com>
2014-12-30 20:23:21 +0800

    copy package_data

--- libshorttext/classifier/learner/liblinear/matlab/linear_model_matlab.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-#ifndef INT64_DEFINED
-typedef int64_t INT64;
-#define INT64_DEFINED
-#endif
-const char *model_to_matlab_structure(mxArray *plhs[], struct model *model_);
-const char *matlab_matrix_to_model(struct model *model_, const mxArray *matlab_struct);
--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from distutils.core import setup, Extension
+from distutils.core import setup
 from distutils.command.install import install as DistutilsInstall
+from distutils.command.clean import clean as Clean
+import shutil
 import os
+from os.path import join
 
 
-class MakeCLibraryInstall(DistutilsInstall):
+class MakeCommand(DistutilsInstall):
     def run(self):
         os.system('make')
         common_dir = 'libshorttext/converter/stemmer'
@@ -21,16 +24,37 @@ class MakeCLibraryInstall(DistutilsInstall):
         DistutilsInstall.run(self)
 
 
+class CleanCommand(Clean):
+    description = "Remove build artifacts from the source tree"
+
+    def run(self):
+        Clean.run(self)
+        if os.path.exists('build'):
+            shutil.rmtree('build')
+        for dirpath, dirnames, filenames in os.walk('libshorttext'):
+            for filename in filenames:
+                if (filename.endswith('.so') or filename.endswith('.pyd') or filename.endswith(
+                        '.dll') or filename.endswith('.pyc')):
+                    os.unlink(os.path.join(dirpath, filename))
+            for dirname in dirnames:
+                if dirname == '__pycache__':
+                    shutil.rmtree(os.path.join(dirpath, dirname))
+
+
 setup(
     name='libshorttext',
     version='1.1',
     packages=['', 'libshorttext', 'libshorttext.analyzer', 'libshorttext.converter', 'libshorttext.converter.stemmer',
               'libshorttext.classifier', 'libshorttext.classifier.learner', 'libshorttext.classifier.learner.liblinear',
               'libshorttext.classifier.learner.liblinear.python'],
+    package_data={'libshorttext': [join('converter', 'stop-words', '*')]},
     url='',
     license='',
     author='',
     author_email='',
     description='',
-    cmdclass={'install': MakeCLibraryInstall}
+    cmdclass={
+        'install': MakeCommand,
+        'clean': CleanCommand,
+    },
 )
